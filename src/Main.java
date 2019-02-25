15766323b103d787baadbb211b8136ecdaf2c860
unknown <martensglen98@gmail.com>
2019-02-25 11:19:15 +0100

    Eerste versie main

---
+++ src/Main.java
@@ -0,0 +1,12 @@
+import canvaswindow.MyCanvasWindow;
+
+public class Main {
+
+	public static void main(String[] args) {
+		java.awt.EventQueue.invokeLater(() -> {
+			 new MyCanvasWindow("Tables").show();
+		});
+		
+	}
+
+}
---
+++ src/canvaswindow/MyCanvasWindow.java
@@ -0,0 +1,10 @@
+package canvaswindow;
+
+public class MyCanvasWindow extends CanvasWindow {
+
+	public MyCanvasWindow(String title) {
+		super(title);
+		// TODO Auto-generated constructor stub
+	}
+
+}
