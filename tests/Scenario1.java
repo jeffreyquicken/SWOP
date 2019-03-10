e88d05bfe255e90d444e8df930b90c9cb4286fa8
unknown <martensglen98@gmail.com>
2019-03-10 17:16:21 +0100

    added some tests

--- tests/Scenario1.java
+++ tests/Scenario1.java
@@ -1,10 +1,14 @@
 
+import Data.Table;
 import org.junit.jupiter.api.Test;
 import UserInterfaceElements.Controller;
 import Data.dataController;
 
+import java.util.List;
+
 import static org.junit.jupiter.api.Assertions.assertEquals;
 
+//Use case: Create table
 public class Scenario1 {
 
     //initialise class variables
@@ -26,8 +30,15 @@ public class Scenario1 {
        int eL = dc.getTableList().size() -1;
 
        assertEquals(origineleLengte, eL);
+    }
 
-
+    @Test
+    public void doubleClickUnderTableAddsNewTableWithCorrectName() {
+        int origineleLengte = dc.getTableList().size();
+        bestuurder.relayMouseEvent(501,100,200,2);
+        List<Table> tble = dc.getTableList();
+        origineleLengte++;
+        assertEquals("Table" +origineleLengte,tble.get(origineleLengte-1).getTableName());
     }
 
 
--- tests/Scenario2.java
+++ tests/Scenario2.java
@@ -1,7 +1,14 @@
+
+import Data.Table;
 import org.junit.jupiter.api.Test;
 import UserInterfaceElements.Controller;
 import Data.dataController;
 
+import java.util.List;
+
+import static org.junit.jupiter.api.Assertions.assertEquals;
+
+//Use case: Edit Table Name
 public class Scenario2 {
     //initialise class variables
     private Controller bestuurder;
@@ -13,4 +20,19 @@ public class Scenario2 {
         dc = bestuurder.getTableDataController();
         bestuurder.setCurrentMode("table");
     }
+
+    @Test
+    public void StandardEditTableNameByPressingEnter() {
+        bestuurder.relayMouseEvent(500,115,60,1);
+        //we should be in table name editing mode right now
+        char car = 'a';
+        bestuurder.relayKeyEvent(400,0,car);
+        bestuurder.relayMouseEvent(500,350,60,1);
+        List<Table> table = dc.getTableList();
+        String tbleName = table.get(0).getTableName();
+        assertEquals("Table 1a",tbleName);
+
+
+    }
+
 }
