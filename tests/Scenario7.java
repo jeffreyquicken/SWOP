167d9e600c6d9b9354ebff90c0d0e3e9e3550443
unknown <martensglen98@gmail.com>
2019-03-14 15:34:54 +0100

    added tests for scenario 6 and 7

--- tests/Scenario7.java
+++ tests/Scenario7.java
@@ -2,6 +2,7 @@
 import org.junit.jupiter.api.Test;
 import Data.dataController;
 import UserInterfaceElements.Controller;
+import static org.junit.jupiter.api.Assertions.assertEquals;
 
 public class Scenario7 {
     //initialise class variables
@@ -12,6 +13,35 @@ public class Scenario7 {
     public Scenario7() {
         bestuurder = new Controller();
         dc = bestuurder.getTableDataController();
-        bestuurder.setCurrentMode("design");
+        bestuurder.setCurrentMode("table");
+        bestuurder.relayMouseEvent(500,115,60,2); // now in rowmode
+        bestuurder.relayKeyEvent(400,17,'o'); // CTRL
+        bestuurder.relayKeyEvent(400,10,'o'); //ENTER
+    }
+
+    //Precondition
+    @Test
+    public void IsInRightMode() {
+        assertEquals("design",bestuurder.getCurrentMode());
+    }
+
+
+    @Test
+    public void UserDeletesColumn() {
+        int originalLen = dc.getTableList().get(0).getColumnNames().size();
+        bestuurder.relayMouseEvent(500,55,120,1); //Select left margin
+        bestuurder.relayKeyEvent(400,127,'o'); //Delete
+        int newLen = dc.getTableList().get(0).getColumnNames().size();
+        assertEquals((originalLen-1),newLen);
+    }
+
+    @Test
+    public void UserClicksLeftMarginAndPressesEscapeBeforeDelete() {
+        int originalLen = dc.getTableList().get(0).getColumnNames().size();
+        bestuurder.relayMouseEvent(500,55,120,1); //Select left margin
+        bestuurder.relayKeyEvent(400,27,'o');//Escape
+        bestuurder.relayKeyEvent(400,127,'o'); //Delete
+        int newLen = dc.getTableList().get(0).getColumnNames().size();
+        assertEquals(originalLen,newLen);
     }
 }
